nova-video-player/aos-Video

commit 85a0a98e992033b8d35b201e5a8d6f12e88b2803

For AndroidTV compliance do not use ACTION_VIEW and call WebUtils.openWebLink that go through a webview if required.

src/leanback/java/com/archos/mediacenter/video/leanback/details/VideoDetailsFragment.java
@@ -338,8 +338,7 @@ public class VideoDetailsFragment extends DetailsFragmentWithLessTopOffset imple
             @Override
             public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item, RowPresenter.ViewHolder rowViewHolder, Row row) {
                 if(item instanceof ScraperTrailer){
-                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, ((ScraperTrailer)item).getUrl());
-                    startActivity(browserIntent);
+                    WebUtils.openWebLink(getActivity(), ((ScraperTrailer)item).getUrl().toString());
                 }
                 else if (item instanceof ScraperImage) {
                     if (row == mPostersRow) {

src/main/java/com/archos/mediacenter/video/info/VideoInfoActivityFragment.java
@@ -103,6 +103,7 @@ import com.archos.mediacenter.video.utils.SubtitlesDownloaderActivity;
 import com.archos.mediacenter.video.utils.TrailerServiceIconFactory;
 import com.archos.mediacenter.video.utils.VideoMetadata;
 import com.archos.mediacenter.video.utils.VideoUtils;
+import com.archos.mediacenter.video.utils.WebUtils;
 import com.archos.mediaprovider.NetworkState;
 import com.archos.mediaprovider.video.VideoStore;
 import com.archos.mediaprovider.video.VideoStoreImportImpl;
@@ -1040,14 +1041,11 @@ public class VideoInfoActivityFragment extends Fragment implements LoaderManager
             // Format TMDB URL with movie ID and preferred language
             final String language = MovieScraper2.getLanguage(getActivity());
             final String tmdbUrl = String.format(getResources().getString(R.string.tmdb_title_url), mTMDBId, language);
-            Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(tmdbUrl));
-            startActivity(it);
-
+            WebUtils.openWebLink(getActivity(), tmdbUrl);
         }
         else if(view == mIMDBIcon){
             final String imdbUrl = getResources().getString(R.string.imdb_title_url) + mIMDBId;
-            Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(imdbUrl));
-            startActivity(it);
+            WebUtils.openWebLink(getActivity(), imdbUrl);
         }
         else if(view == mPosterImageView){
             if(!mCurrentVideo.hasScraperData())
@@ -1637,8 +1635,7 @@ public class VideoInfoActivityFragment extends Fragment implements LoaderManager
                         button.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
-                                Intent browserIntent = new Intent(Intent.ACTION_VIEW, trailer.getUrl());
-                                startActivity(browserIntent);
+                                WebUtils.openWebLink(getActivity(), trailer.getUrl().toString());
                             }
                         });
                         button.setText(trailer.mName);

src/main/java/com/archos/mediacenter/video/utils/StoreRatingDialogBuilder.java
@@ -61,7 +61,7 @@ public class StoreRatingDialogBuilder{
                     context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse((googlePlay ? "market://details?id=" : "amzn://apps/android?p=") +context.getPackageName())));
                 } catch (ActivityNotFoundException e1) {
                     try {
-                        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse((googlePlay ? "http://play.google.com/store/apps/details?id=" : "http://www.amazon.com/gp/mas/dl/android?p=") +context.getPackageName())));
+                        WebUtils.openWebLink(context, googlePlay ? "http://play.google.com/store/apps/details?id=" : "http://www.amazon.com/gp/mas/dl/android?p=" +context.getPackageName());
                     } catch (ActivityNotFoundException e2) {
                     }
                 }

src/main/java/com/archos/mediacenter/video/utils/VideoPreferencesActivity.java
@@ -47,16 +47,17 @@ public class VideoPreferencesActivity extends AppCompatActivity {
             ((VideoPreferencesFragment)getFragmentManager().findFragmentById(R.id.preferencesFragment)).launchPurchase();
         } }
     public void videoPreferenceOsClick(View v) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.opensubtitles.org")).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        //remove because not appropriate links displayed
+        //WebUtils.openWebLink(v.this.getActivity,"http://www.opensubtitles.org");
     }
     public void videoPreferenceTmdbClick(View v) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.themoviedb.org")).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        WebUtils.openWebLink(this,"http://www.themoviedb.org");
     }
     public void videoPreferenceTvdbClick(View v) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://thetvdb.com")).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        WebUtils.openWebLink(this,"http://thetvdb.com");
     }
-    public void videoPreferenceTraktClick(View b) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://trakt.tv/")).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    public void videoPreferenceTraktClick(View v) {
+        WebUtils.openWebLink(this,"http://trakt.tv/");
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {

src/main/java/com/archos/mediacenter/video/browser/BrowserCategory.java
@@ -45,6 +45,7 @@ import com.archos.mediacenter.upnp.UpnpAvailability;
 import com.archos.mediacenter.video.R;
 import com.archos.mediacenter.video.info.VideoInfoActivity;
 import com.archos.mediacenter.video.player.PrivateMode;
+import com.archos.mediacenter.video.utils.WebUtils;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -274,9 +275,7 @@ abstract public class BrowserCategory extends ListFragment {
                 setSelection(mSelectedItemId); //restore selection
             }
             else if (item.text == R.string.help_faq){
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("https://www.archos.com/support/faq_videoplayer/index.htm"));
-                startActivity(i);
+                WebUtils.openWebLink(getActivity(),"https://www.archos.com/support/faq_videoplayer/index.htm");
             }
             else if(item.text  == R.string.activate_private_mode || item.text  == R.string.deactivate_private_mode){
                 if (!PrivateMode.isActive() && PrivateMode.canShowDialog(getActivity())) {

commit b555c23978790112987ebe260749331158b5e7d2

Youtube trailers not displayed in youtube application following a regression introduced by https://github.com/nova-video-player/aos-Video/commit/85a0a98e992033b8d35b201e5a8d6f12e88b2803 Fix https://github.com/nova-video-player/aos-AVP/issues/33

src/leanback/java/com/archos/mediacenter/video/leanback/details/VideoDetailsFragment.java
@@ -342,7 +342,10 @@ public class VideoDetailsFragment extends DetailsFragmentWithLessTopOffset imple
             @Override
             public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item, RowPresenter.ViewHolder rowViewHolder, Row row) {
                 if(item instanceof ScraperTrailer){
-                    WebUtils.openWebLink(getActivity(), ((ScraperTrailer)item).getUrl().toString());
+                    // Breaks AndroidTV acceptance but needed to launch scraper in Youtube app instead of browser
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, ((ScraperTrailer)item).getUrl());
+                    startActivity(browserIntent);
+                    //WebUtils.openWebLink(getActivity(), ((ScraperTrailer)item).getUrl().toString());
                 }
                 else if (item instanceof ScraperImage) {
                     if (row == mPostersRow) {

src/main/java/com/archos/mediacenter/video/info/VideoInfoActivityFragment.java
@@ -96,7 +96,7 @@ import com.archos.mediacenter.video.utils.SubtitlesDownloaderActivity;
 import com.archos.mediacenter.video.utils.TrailerServiceIconFactory;
 import com.archos.mediacenter.video.utils.VideoMetadata;
 import com.archos.mediacenter.video.utils.VideoUtils;
-import com.archos.mediacenter.video.utils.WebUtils;
+//import com.archos.mediacenter.video.utils.WebUtils;
 import com.archos.mediaprovider.NetworkState;
 import com.archos.mediaprovider.video.VideoStore;
 import com.archos.mediaprovider.video.VideoStoreImportImpl;
@@ -1026,11 +1026,17 @@ public class VideoInfoActivityFragment extends Fragment implements LoaderManager
             // Format TMDB URL with movie ID and preferred language
             final String language = MovieScraper2.getLanguage(getActivity());
             final String tmdbUrl = String.format(getResources().getString(R.string.tmdb_title_url), mTMDBId, language);
-            WebUtils.openWebLink(getActivity(), tmdbUrl);
+            // Breaks AndroidTV acceptance
+            Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(tmdbUrl));
+            startActivity(it);
+            //WebUtils.openWebLink(getActivity(), tmdbUrl);
         }
         else if(view == mIMDBIcon){
             final String imdbUrl = getResources().getString(R.string.imdb_title_url) + mIMDBId;
-            WebUtils.openWebLink(getActivity(), imdbUrl);
+            // Breaks AndroidTV acceptance but required to open link in app instead of browser
+            Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(imdbUrl));
+            startActivity(it);
+            //WebUtils.openWebLink(getActivity(), imdbUrl);
         }
         else if(view == mPosterImageView){
             if(!mCurrentVideo.hasScraperData())
@@ -1620,7 +1626,10 @@ public class VideoInfoActivityFragment extends Fragment implements LoaderManager
                         button.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
-                                WebUtils.openWebLink(getActivity(), trailer.getUrl().toString());
+                                // Breaks AndroidTV acceptance but required to open link in app instead of browser
+                                Intent browserIntent = new Intent(Intent.ACTION_VIEW, trailer.getUrl());
+                                startActivity(browserIntent);
+                                //WebUtils.openWebLink(getActivity(), trailer.getUrl().toString());
                             }
                         });
                         button.setText(trailer.mName);

src/main/java/com/archos/mediacenter/video/utils/StoreRatingDialogBuilder.java
@@ -58,7 +58,9 @@ public class StoreRatingDialogBuilder{
                     context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse((googlePlay ? "market://details?id=" : "amzn://apps/android?p=") +context.getPackageName())));
                 } catch (ActivityNotFoundException e1) {
                     try {
-                        WebUtils.openWebLink(context, googlePlay ? "http://play.google.com/store/apps/details?id=" : "http://www.amazon.com/gp/mas/dl/android?p=" +context.getPackageName());
+                        // Breaks AndroidTV acceptance but required to open link in app instead of browser
+                        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse((googlePlay ? "http://play.google.com/store/apps/details?id=" : "http://www.amazon.com/gp/mas/dl/android?p=") +context.getPackageName())));
+                        //WebUtils.openWebLink(context, googlePlay ? "http://play.google.com/store/apps/details?id=" : "http://www.amazon.com/gp/mas/dl/android?p=" +context.getPackageName());
                     } catch (ActivityNotFoundException e2) {
                     }
                 }

src/main/java/com/archos/mediacenter/video/utils/VideoPreferencesActivity.java
@@ -48,15 +48,15 @@ public class VideoPreferencesActivity extends AppCompatActivity {
         } }
     public void videoPreferenceOsClick(View v) {
         //remove because not appropriate links displayed
-        //WebUtils.openWebLink(v.this.getActivity,"https://www.opensubtitles.org");
+        WebUtils.openWebLink(this,"https://www.opensubtitles.org");
     }
     public void videoPreferenceTmdbClick(View v) {
         // Breaks AndroidTV acceptance: text is cut on edges
-        //WebUtils.openWebLink(this,"https://www.themoviedb.org");
+        WebUtils.openWebLink(this,"https://www.themoviedb.org");
     }
     public void videoPreferenceTvdbClick(View v) {
         // Breaks AndroidTV acceptance: contains non fullscreen ads
-        //WebUtils.openWebLink(this,"https://thetvdb.com");
+        WebUtils.openWebLink(this,"https://thetvdb.com");
     }
     public void videoPreferenceTraktClick(View v) {
         WebUtils.openWebLink(this,"https://trakt.tv/");

src/main/java/com/archos/mediacenter/video/utils/VideoPreferencesFragment.java
@@ -711,15 +711,15 @@ public class VideoPreferencesFragment extends PreferenceFragment implements OnSh
     }
     public void videoPreferenceOsClick() {
         // Breaks AndroidTV acceptance: inappropriate content TV-AA rating on opensubtitles web site
-        // WebUtils.openWebLink(getActivity(), "https://www.opensubtitles.org/support");
+        WebUtils.openWebLink(getActivity(), "https://www.opensubtitles.org/support");
     }
     public void videoPreferenceTmdbClick() {
         // Breaks AndroidTV acceptance: text is cut on edges
-        //WebUtils.openWebLink(getActivity(), "https://www.themoviedb.org/faq/general");
+        WebUtils.openWebLink(getActivity(), "https://www.themoviedb.org/faq/general");
     }
     public void videoPreferenceTvdbClick() {
         // Breaks AndroidTV acceptance: contains non fullscreen ads
-        //WebUtils.openWebLink(getActivity(), "https://thetvdb.com/donate");
+        WebUtils.openWebLink(getActivity(), "https://thetvdb.com/donate");
     }
     public void videoPreferenceTraktClick() {
         WebUtils.openWebLink(getActivity(), "https://trakt.tv/about");