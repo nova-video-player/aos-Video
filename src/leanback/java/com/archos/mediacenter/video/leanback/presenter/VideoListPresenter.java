f4b9d90f879e66ddf724ffc0a538728dc48afac7
Marc de Courville <marc@courville.org>
2019-02-06 20:09:23 +0100

    Make VideoListPresenter able to cope with tvshows even though this is not technically a Video (class naming consistency issue). Fixes https://github.com/nova-video-player/aos-AVP/issues/44

--- src/leanback/java/com/archos/mediacenter/video/leanback/presenter/VideoListPresenter.java
+++ src/leanback/java/com/archos/mediacenter/video/leanback/presenter/VideoListPresenter.java
@@ -1,4 +1,4 @@
-// Copyright 2017 Archos SA
+// Copyright 2017 Archos SA, 2019 Courville Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,14 +17,16 @@ package com.archos.mediacenter.video.leanback.presenter;
 import android.net.Uri;
 import android.view.View;
 
+import com.archos.environment.ArchosUtils;
 import com.archos.mediacenter.video.R;
 import com.archos.mediacenter.video.browser.adapters.object.Episode;
 import com.archos.mediacenter.video.browser.adapters.object.Movie;
+import com.archos.mediacenter.video.browser.adapters.object.Tvshow;
 import com.archos.mediacenter.video.browser.adapters.object.Video;
 import com.archos.mediacenter.video.player.PlayerActivity;
 
 /**
- * ListPresenter for Video objects
+ * ListPresenter for Video objects including Video, Episodes, Tvshow (though not technically a Video)
  * Created by vapillon on 10/04/15.
  */
 public class VideoListPresenter extends ListPresenter {
@@ -38,50 +40,52 @@ public class VideoListPresenter extends ListPresenter {
 
     @Override
     public void onBindListViewHolder(ListViewHolder viewHolder, Object item) {
-        ListViewHolder vh = (ListViewHolder)viewHolder;
-        Video video = (Video)item;
-        final Uri posterUri = video.getPosterUri();
-        if (posterUri!=null) {
-            vh.updateImageViewPoster(posterUri, video.getId());
-        }
-        else {
-            vh.updateImageViewThumbnail(video.getId());
-        }
+        ListViewHolder vh = (ListViewHolder) viewHolder;
 
-        if (video instanceof Episode) {
-            Episode episode = (Episode)video;
-            vh.setTitleText(vh.view.getResources().getString(R.string.leanback_episode_name_for_browser_list,
-                    episode.getShowName(), episode.getSeasonNumber(), episode.getEpisodeNumber(), episode.getEpisodeName()));
-            if (mDisplayFileName) {
-                vh.setContentText(video.getFilenameNonCryptic());
-                vh.setContentTextVisibility(View.VISIBLE);
-            } else {
+        if (item instanceof Movie || item instanceof Episode || item instanceof Video) {
+            Video video = (Video) item;
+            final Uri posterUri = video.getPosterUri();
+            if (posterUri != null)
+                vh.updateImageViewPoster(posterUri, video.getId());
+            else
+                vh.updateImageViewThumbnail(video.getId());
+            if (item instanceof Episode) {
+                Episode episode = (Episode) item;
+                vh.setTitleText(vh.view.getResources().getString(R.string.leanback_episode_name_for_browser_list,
+                        episode.getShowName(), episode.getSeasonNumber(), episode.getEpisodeNumber(), episode.getEpisodeName()));
+            } else if (item instanceof Movie)
+                vh.setTitleText(video.getName());
+            else // Non-indexed or non-scraped video
+                vh.setTitleText(video.getFilenameNonCryptic());
+            if (item instanceof Movie || item instanceof Episode)
+                if (mDisplayFileName) {
+                    vh.setContentText(video.getFilenameNonCryptic());
+                    vh.setContentTextVisibility(View.VISIBLE);
+                } else {
+                    vh.setContentTextVisibility(View.GONE);
+                }
+            else // Non-indexed or non-scraped video
                 vh.setContentTextVisibility(View.GONE);
-            }
-        }
-        else if (video instanceof Movie) {
-            vh.setTitleText(video.getName());
-            if (mDisplayFileName) {
-                vh.setContentText(video.getFilenameNonCryptic());
-                vh.setContentTextVisibility(View.VISIBLE);
+            int resumeMs = video.getResumeMs();
+            if (resumeMs == 0 || resumeMs == PlayerActivity.LAST_POSITION_UNKNOWN) {
+                vh.setResumeInPercent(0f);
+            } else if (resumeMs == PlayerActivity.LAST_POSITION_END) {
+                vh.setResumeInPercent(100f);
             } else {
-                vh.setContentTextVisibility(View.GONE);
+                vh.setResumeInPercent(100 * resumeMs / (float) video.getDurationMs());
             }
+            vh.setWatched(video.isWatched());
+        } else if (item instanceof Tvshow) {
+            Tvshow tvshow = (Tvshow) item;
+            final Uri posterUri = tvshow.getPosterUri();
+            if (posterUri != null) {
+                vh.updateImageViewPoster(posterUri, tvshow.getTvshowId());
+            } else {
+                vh.updateImageViewThumbnail(tvshow.getTvshowId());
+            }
+            vh.setTitleText(tvshow.getName());
+            vh.setContentText(tvshow.getCountString(ArchosUtils.getGlobalContext()));
+            vh.setContentTextVisibility(View.VISIBLE);
         }
-        else {// Non-indexed or non-scraped video
-            vh.setTitleText(video.getFilenameNonCryptic());
-            vh.setContentTextVisibility(View.GONE);
-        }
-
-        int resumeMs = video.getResumeMs();
-        if (resumeMs == 0 || resumeMs == PlayerActivity.LAST_POSITION_UNKNOWN) {
-            vh.setResumeInPercent(0f);
-        } else if (resumeMs == PlayerActivity.LAST_POSITION_END) {
-            vh.setResumeInPercent(100f);
-        } else {
-            vh.setResumeInPercent(100 * resumeMs/(float)video.getDurationMs());
-        }
-
-        vh.setWatched(video.isWatched());
     }
 }
