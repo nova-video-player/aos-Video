ca7cbd8add617b72497497aa0ef0e530e185346e
persi-persu <persi.persu@gmail.com>
2019-02-18 20:29:11 +0100

    UI tweaks (tv shows details)

--- src/leanback/java/com/archos/mediacenter/video/leanback/tvshow/TvshowMoreDetailsDescriptionPresenter.java
+++ src/leanback/java/com/archos/mediacenter/video/leanback/tvshow/TvshowMoreDetailsDescriptionPresenter.java
@@ -14,20 +14,80 @@
 
 package com.archos.mediacenter.video.leanback.tvshow;
 
-import android.support.v17.leanback.widget.AbstractDetailsDescriptionPresenter;
+import android.support.v17.leanback.widget.Presenter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.archos.mediacenter.video.R;
 import com.archos.mediascraper.ShowTags;
 
-public class TvshowMoreDetailsDescriptionPresenter extends AbstractDetailsDescriptionPresenter {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TvshowMoreDetailsDescriptionPresenter extends Presenter {
 
     private static final String TAG = "TvshowMoreDetailsDescriptionPresenter";
+    
+    public static class ViewHolder extends Presenter.ViewHolder {
+        final TextView mTitle;
+        final TextView mDate;
+        final TextView mRating;
 
+        public ViewHolder(final View view) {
+            super(view);
+
+            mTitle = (TextView) view.findViewById(android.support.v17.leanback.R.id.lb_details_description_title);
+            mDate = (TextView) view.findViewById(R.id.date);
+            mRating = (TextView) view.findViewById(R.id.rating);
+        }
+    }
+    
     @Override
-    protected void onBindDescription(ViewHolder vh, Object item) {
+    public final ViewHolder onCreateViewHolder(ViewGroup parent) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.leanback_tvshow_more_details_description, parent, false);
+        
+        return new ViewHolder(v);
+    }
+    
+    @Override
+    public final void onBindViewHolder(Presenter.ViewHolder viewHolder, Object item) {
+        ViewHolder vh = (ViewHolder)viewHolder;
         ShowTags tags = (ShowTags)item;
 
-        vh.getTitle().setText(tags.getTitle());
-        vh.getSubtitle().setText("");
-        vh.getBody().setText("");
+        vh.mTitle.setText(tags.getTitle());
+        setTextOrSetGoneIfEmpty(vh.mDate, getYearFormatted(tags.getPremiered()));
+        setTextOrSetGoneIfZero(vh.mRating, tags.getRating());
+    }
+    
+    @Override
+    public void onUnbindViewHolder(Presenter.ViewHolder viewHolder) {
+        ViewHolder vh = (ViewHolder) viewHolder;
+    }
+    
+    private void setTextOrSetGoneIfEmpty(TextView mTextView, String text) {
+        if (text==null || text.isEmpty()) {
+            mTextView.setVisibility(View.GONE);
+        } else {
+            mTextView.setText(text);
+            mTextView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setTextOrSetGoneIfZero(TextView mTextView, float value) {
+        if (value == 0f) {
+            mTextView.setVisibility(View.GONE);
+        } else {
+            mTextView.setText( Float.toString(value));
+            mTextView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private String getYearFormatted(Date date) {
+        if (date != null) {
+            return new SimpleDateFormat("yyyy").format(date);
+        }
+        return null;
     }
 }
