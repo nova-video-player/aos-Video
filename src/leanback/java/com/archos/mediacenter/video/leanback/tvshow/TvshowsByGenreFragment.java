47652cbd6c553d81176594f7395af4b9583c7cd4
persi-persu <persi.persu@gmail.com>
2019-02-27 15:40:25 +0100

    Change tv show sort

--- src/leanback/java/com/archos/mediacenter/video/leanback/tvshow/TvshowsByGenreFragment.java
+++ src/leanback/java/com/archos/mediacenter/video/leanback/tvshow/TvshowsByGenreFragment.java
@@ -40,9 +40,10 @@ public class TvshowsByGenreFragment extends TvshowsByFragment {
         //sortOrderIndexer.put(2, new TvshowsSortOrderEntry(R.string.sort_by_duration_asc,    SortOrder.DURATION.getAsc()));
         //sortOrderIndexer.put(3, new TvshowsSortOrderEntry(R.string.sort_by_rating_asc,      SortOrder.SCRAPER_M_RATING.getDesc()));
         // TODO: look in BrowserAllTvShows.java for all the sort possibilities
-        sortOrderIndexer.put(0, new TvshowsSortOrderEntry(R.string.sort_by_name_asc,        VideoStore.Video.VideoColumns.SCRAPER_S_NAME + " ASC"));
+        sortOrderIndexer.put(0, new TvshowsSortOrderEntry(R.string.sort_by_name_asc,        VideoStore.Video.VideoColumns.SCRAPER_TITLE + " ASC"));
         sortOrderIndexer.put(1, new TvshowsSortOrderEntry(R.string.sort_by_date_added_desc, "max(" + VideoStore.Video.VideoColumns.DATE_ADDED + ") DESC"));
-        sortOrderIndexer.put(2, new TvshowsSortOrderEntry(R.string.sort_by_rating_asc,      "IFNULL(" + VideoStore.Video.VideoColumns.SCRAPER_S_RATING + ", 0) DESC"));
+        sortOrderIndexer.put(2, new TvshowsSortOrderEntry(R.string.sort_by_date_premiered_desc,       VideoStore.Video.VideoColumns.SCRAPER_S_PREMIERED + " DESC"));
+        sortOrderIndexer.put(3, new TvshowsSortOrderEntry(R.string.sort_by_rating_asc,      "IFNULL(" + VideoStore.Video.VideoColumns.SCRAPER_S_RATING + ", 0) DESC"));
     }
 
     @Override
