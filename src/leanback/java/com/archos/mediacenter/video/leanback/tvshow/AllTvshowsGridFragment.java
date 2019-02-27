367803ae3190134a97b15b6409c8bedc9292d08d
persi-persu <persi.persu@gmail.com>
2019-02-27 15:37:37 +0100

    Change all tv show row sort setting
    - Change tv show sort

--- src/leanback/java/com/archos/mediacenter/video/leanback/tvshow/AllTvshowsGridFragment.java
+++ src/leanback/java/com/archos/mediacenter/video/leanback/tvshow/AllTvshowsGridFragment.java
@@ -69,11 +69,11 @@ public class AllTvshowsGridFragment extends MyVerticalGridFragment implements Lo
     private CharSequence[] mSortOrderEntries;
     private BackgroundManager bgMngr = null;
 
-    private static SparseArray<TvshowsSortOrderEntry> sortOrderIndexer = new SparseArray<TvshowsSortOrderEntry>();
+    public static SparseArray<TvshowsSortOrderEntry> sortOrderIndexer = new SparseArray<TvshowsSortOrderEntry>();
     static {
         sortOrderIndexer.put(0, new TvshowsSortOrderEntry(R.string.sort_by_name_asc,        VideoStore.Video.VideoColumns.SCRAPER_TITLE + " ASC"));
         sortOrderIndexer.put(1, new TvshowsSortOrderEntry(R.string.sort_by_date_added_desc, "max(" + VideoStore.Video.VideoColumns.DATE_ADDED + ") DESC"));
-        sortOrderIndexer.put(2, new TvshowsSortOrderEntry(R.string.sort_by_year_desc,       VideoStore.Video.VideoColumns.SCRAPER_S_PREMIERED + " DESC"));
+        sortOrderIndexer.put(2, new TvshowsSortOrderEntry(R.string.sort_by_date_premiered_desc,       VideoStore.Video.VideoColumns.SCRAPER_S_PREMIERED + " DESC"));
         sortOrderIndexer.put(3, new TvshowsSortOrderEntry(R.string.sort_by_rating_asc,      "IFNULL(" + VideoStore.Video.VideoColumns.SCRAPER_S_RATING + ", 0) DESC"));
     }
 
