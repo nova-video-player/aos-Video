90ea24af74c44fcb5a829c1cc41cdf908b704682
Persi Persu <persi.persu@gmail.com>
2019-03-13 23:11:44 +0100

    Add unindex local folders

--- src/leanback/java/com/archos/mediacenter/video/leanback/filebrowsing/LocalListingFragment.java
+++ src/leanback/java/com/archos/mediacenter/video/leanback/filebrowsing/LocalListingFragment.java
@@ -14,13 +14,152 @@
 
 package com.archos.mediacenter.video.leanback.filebrowsing;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.archos.customizedleanback.widget.MyTitleView;
+import com.archos.mediacenter.utils.BlacklistedDbAdapter;
+import com.archos.mediacenter.video.R;
+import com.archos.mediaprovider.ArchosMediaIntent;
+import com.archos.mediaprovider.video.VideoStoreImportService;
+
 /**
  * Created by vapillon on 17/04/15.
  */
 public class LocalListingFragment extends ListingFragment {
 
+    private static final String TAG = "LocalListingFragment";
+
+    private long mBlacklistedId;
+    private boolean mAnAncestorIsBlacklisted;
+
     @Override
     protected  ListingFragment instantiateNewFragment() {
         return new LocalListingFragment();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        updateBlacklistedState();
+    }
+
+    private final View.OnClickListener mOrbClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (isBlacklisted()) {
+                deleteBlacklisted();
+            } else {
+                createBlacklisted();
+            }
+        }
+    };
+
+    /**
+     * Check if the current folder is blacklisted or if one of his ancestor is.
+     * Update the available actions (orbs) accordingly
+     */
+    protected void updateBlacklistedState() {
+        final String currentUri = mUri.toString();
+
+        Cursor c = BlacklistedDbAdapter.VIDEO.queryAllBlacklisteds(getActivity());
+        final int pathColumn = c.getColumnIndexOrThrow(BlacklistedDbAdapter.KEY_PATH);
+        final int idColumn = c.getColumnIndexOrThrow(BlacklistedDbAdapter.KEY_ROWID);
+
+        mAnAncestorIsBlacklisted = false; // reset value
+        mBlacklistedId = -1; // reset value
+
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            String blacklistedPath = c.getString(pathColumn);
+            if (blacklistedPath!=null && blacklistedPath.equals(mUri.toString())) {
+                mBlacklistedId = c.getLong(idColumn);
+            }
+            // Check if this blacklisted Uri is ancestor of the current Uri
+            if (currentUri.startsWith(blacklistedPath)) {
+                mAnAncestorIsBlacklisted = true;
+                //Log.d(TAG, "mAnAncestorIsBlacklisted="+mAnAncestorIsBlacklisted);
+                break;
+            }
+            c.moveToNext();
+        }
+        c.close();
+
+        updateOrbIcon();
+    }
+
+    private boolean isBlacklisted() {
+        return mBlacklistedId>=0;
+    }
+
+    private void updateOrbIcon() {
+        final MyTitleView titleView = getTitleView();
+        if (isBlacklisted()) {
+            titleView.setOrb3IconResId(R.drawable.orb_plus);
+            titleView.setOnOrb3ClickedListener(mOrbClickListener);
+            titleView.setOnOrb3Description(getString(R.string.add_to_indexed_folders));
+        }
+        else {
+            if (!canBeUnindexed()) {
+                titleView.setOnOrb3ClickedListener(null); // set null listener to hide the orb
+            } else {
+                titleView.setOrb3IconResId(R.drawable.orb_minus);
+                titleView.setOnOrb3ClickedListener(mOrbClickListener);
+                titleView.setOnOrb3Description(getString(R.string.remove_from_indexed_folders));
+            }
+        }
+    }
+
+    /**
+     * Returns true if this folder can be indexed
+     * @return
+     */
+    protected boolean canBeUnindexed() {
+        return !mAnAncestorIsBlacklisted;
+    }
+
+    /** Add current Uri to the blacklisted list */
+    protected void createBlacklisted() {
+
+        String blacklistedPath = mUri.toString();
+        String blacklistedName = getArguments().getString(ARG_TITLE)!=null?getArguments().getString(ARG_TITLE):mUri.getLastPathSegment();
+        boolean result = BlacklistedDbAdapter.VIDEO.addBlacklisted(getActivity(), new BlacklistedDbAdapter.Blacklisted(blacklistedPath));
+
+        if (result) {
+            Toast.makeText(getActivity(), getString(R.string.indexed_folder_removed, blacklistedName), Toast.LENGTH_SHORT).show();
+            // Send a scan request to MediaScanner
+            Intent serviceIntent = new Intent(getActivity(), VideoStoreImportService.class);
+            serviceIntent.setAction(ArchosMediaIntent.ACTION_VIDEO_SCANNER_METADATA_UPDATE);
+            serviceIntent.setData(mUri);
+            getActivity().startService(serviceIntent);
+        }
+        else {
+            Toast.makeText(getActivity(), R.string.error, Toast.LENGTH_SHORT).show();
+        }
+        updateBlacklistedState();
+    }
+
+    /** Remove current Uri from the blacklisted list */
+    private void deleteBlacklisted() {
+        String blacklistedPath = mUri.toString();
+        String blacklistedName = getArguments().getString(ARG_TITLE)!=null?getArguments().getString(ARG_TITLE):mUri.getLastPathSegment();
+
+        boolean result = BlacklistedDbAdapter.VIDEO.deleteBlacklisted(getActivity(), mUri.toString());
+        if (result) {
+            Toast.makeText(getActivity(), getString(R.string.indexed_folder_added, blacklistedName), Toast.LENGTH_SHORT).show();
+            // Tell MediaScanner to remove the videos from this directory
+            Intent serviceIntent = new Intent(getActivity(), VideoStoreImportService.class);
+            serviceIntent.setAction(ArchosMediaIntent.ACTION_VIDEO_SCANNER_METADATA_UPDATE);
+            serviceIntent.setData(mUri);
+            getActivity().startService(serviceIntent);
+        }
+        else {
+            Toast.makeText(getActivity(), R.string.error, Toast.LENGTH_SHORT).show();
+        }
+        updateBlacklistedState();
     }
 }
