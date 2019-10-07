dc029925fd4e90609ea75738c3ad947f58021a43
Marc de Courville <marc@courville.org>
2019-10-07 13:30:43 +0200

    Replace superseded NovaPreferenceFragmentCompat hack by VideoPreferencesFragment

--- src/main/java/com/archos/mediacenter/video/utils/VideoPreferencesLicencesFragment.java
+++ src/main/java/com/archos/mediacenter/video/utils/VideoPreferencesLicencesFragment.java
@@ -22,7 +22,7 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import com.archos.mediacenter.video.R;
 
-public class VideoPreferencesLicencesFragment extends NovaPreferenceFragmentCompat {
+public class VideoPreferencesLicencesFragment extends VideoPreferencesFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
