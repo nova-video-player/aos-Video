cec5c28da202895ed812d44622c10ef9ca1c96b9
bronnel <austin.316scsa@yahoo.com>
2022-03-01 18:35:53 +0300

    add SeasonsData POJO class

---
+++ src/main/java/com/archos/mediacenter/video/browser/adapters/SeasonsData.java
@@ -0,0 +1,32 @@
+package com.archos.mediacenter.video.browser.adapters;
+
+public class SeasonsData {
+    String seasonNumber,seasonPlot, seasonName;
+
+    public SeasonsData() {
+    }
+
+    public String getSeasonPlot() {
+        return seasonPlot;
+    }
+
+    public void setSeasonPlot(String seasonPlot) {
+        this.seasonPlot = seasonPlot;
+    }
+
+    public String getSeasonName() {
+        return seasonName;
+    }
+
+    public void setSeasonName(String seasonName) {
+        this.seasonName = seasonName;
+    }
+
+    public String getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(String seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+}
