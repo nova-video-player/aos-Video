4bd73388fa5acc6be630812f260b07b21fd6da5c
bronnel <austin.316scsa@yahoo.com>
2022-03-04 11:46:03 +0300

    if series created by is null hide its container

--- src/main/java/com/archos/mediacenter/video/browser/BrowserByIndexedVideos/BrowserWithShowHeader.java
+++ src/main/java/com/archos/mediacenter/video/browser/BrowserByIndexedVideos/BrowserWithShowHeader.java
@@ -39,6 +39,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -368,6 +369,10 @@ public abstract class BrowserWithShowHeader extends CursorBrowserByVideo  {
             // Utilizing the unused series director as a pipeline for series created by tag
             TextView createdBy = (TextView) mHeaderView.findViewById(R.id.created_by);
             createdBy.setText(tags.getDirectorsFormatted());
+            LinearLayout createdbyContainer = (LinearLayout) mHeaderView.findViewById(R.id.created_by_container);
+            if (tags.getDirectorsFormatted() == null)
+                createdbyContainer.setVisibility(View.GONE);
+
 
             TextView network = (TextView) mHeaderView.findViewById(R.id.network);
             network.setText(show.getStudio());
