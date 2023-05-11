55d2f6196ce8a2a9493c136031af414035e09263
Courville Software <software@courville.org>
2023-05-11 08:17:02 +0200

    do not setup bouncycasle

--- src/main/java/com/archos/mediacenter/video/CustomApplication.java
+++ src/main/java/com/archos/mediacenter/video/CustomApplication.java
@@ -243,8 +243,6 @@ public class CustomApplication extends Application {
         // must be done after context is available
         log = LoggerFactory.getLogger(CustomApplication.class);
 
-        setupBouncyCastle();
-
         // must be done before sambaDiscovery otherwise no context for jcifs
         new Thread(() -> {
             // create instance of jcifsUtils in order to pass context and initial preference
@@ -613,22 +611,5 @@ public class CustomApplication extends Application {
                 }
             })
             .show();
-    }
-
-    private void setupBouncyCastle() {
-        final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
-        if (provider == null) {
-            return;
-        }
-        if (provider.getClass().equals(BouncyCastleProvider.class)) {
-            // BC with same package name, shouldn't happen in real life.
-            return;
-        }
-        // Android registers its own BC provider. As it might be outdated and might not include
-        // all needed ciphers, we substitute it with a known BC bundled in the app.
-        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
-        // of that it's possible to have another BC implementation loaded in VM.
-        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
-        Security.insertProviderAt(new BouncyCastleProvider(), 1);
     }
 }
