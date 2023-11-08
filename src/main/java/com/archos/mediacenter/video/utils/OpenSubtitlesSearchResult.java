48f70d7908953318bdc68b664a6e0d4e2e1e2b77
Courville Software <software@courville.org>
2023-11-08 19:47:07 +0100

    OpenSubtitlesSearchResult: more fields

--- src/main/java/com/archos/mediacenter/video/utils/OpenSubtitlesSearchResult.java
+++ src/main/java/com/archos/mediacenter/video/utils/OpenSubtitlesSearchResult.java
@@ -15,9 +15,19 @@
 package com.archos.mediacenter.video.utils;
 
 public class OpenSubtitlesSearchResult {
+    private String id;
     private String fileId;
     private String fileName;
     private String language;
+    private String release; // release name
+    private String movie_name; // scraped movie or episode name
+    private String parent_title; // show name
+    private Boolean moviehash_match; // moviehash_match
+    private Integer season_number; // season, 0 if movie
+    private Integer episode_number; // episode, 0 if movie
+    private String feature_type; // movie or episode
+
+    public OpenSubtitlesSearchResult() {}
 
     public OpenSubtitlesSearchResult(String fileId, String fileName, String language) {
         this.fileId = fileId;
@@ -25,12 +35,43 @@ public class OpenSubtitlesSearchResult {
         this.language = language;
     }
 
+    public String getId() { return id; }
     public String getFileId() { return fileId; }
     public String getFileName() { return fileName; }
     public String getLanguage() { return language; }
+    public String getRelease() { return release; }
+    public String getMovieName() { return movie_name; }
+    public String getParentTitle() { return parent_title; }
+    public Boolean getMoviehashMatch() { return moviehash_match; }
+    public Integer getSeasonNumber() { return season_number; }
+    public Integer getEpisodeNumber() { return episode_number; }
+    public String getFeatureType() { return feature_type; }
+
+    public void setId(String id) { this.id = id; }
+    public void setRelease(String release) { this.release = release; }
+    public void setMovieName(String movie_name) { this.movie_name = movie_name; }
+    public void setParentTitle(String parent_title) { this.parent_title = parent_title; }
+    public void setMoviehashMatch(Boolean moviehash_match) { this.moviehash_match = moviehash_match; }
+    public void setSeasonNumber(Integer season_number) { this.season_number = season_number; }
+    public void setEpisodeNumber(Integer episode_number) { this.episode_number = episode_number; }
+    public void setFeatureType(String feature_type) { this.feature_type = feature_type; }
+    public void setFileName(String fileName) { this.fileName = fileName; }
+    public void setFileId(String fileId) { this.fileId = fileId; }
+    public void setLanguage(String language) { this.language = language; }
 
     @Override
     public String toString() {
-        return "{" + "fileId='" + fileId + "', fileName='" + fileName + "', language='" + language + "'}";
+        return "{" +
+                "fileId='" + fileId + '\'' +
+                ", fileName='" + fileName + '\'' +
+                ", language='" + language + '\'' +
+                ", release='" + release + '\'' +
+                ", movie_name='" + movie_name + '\'' +
+                ", parent_title='" + parent_title + '\'' +
+                ", moviehash_match=" + moviehash_match +
+                ", season_number=" + season_number +
+                ", episode_number=" + episode_number +
+                ", feature_type='" + feature_type + '\'' +
+                '}';
     }
 }
