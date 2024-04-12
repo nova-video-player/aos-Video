13131304219637573fe944d065f14cff050d1f40
Courville Software <software@courville.org>
2024-04-12 15:00:58 +0200

    attempt to comply with google requirements in highres

---
+++ icon_sources/convert-ic_launcher.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# src must be 1024x1024 size
+srcn=ic_launcher
+srca=ic_launcher_foreground
+
+# Define arrays for resolutions and directory names
+resolutions=(256 384 512 768 1024)
+#resolutions=(80 120 160 240 320)
+densities=("mdpi" "hdpi" "xhdpi" "xxhdpi" "xxxhdpi")
+
+# Create densities if they don't exist
+for dir in "${densities[@]}"
+do
+  mkdir -p "res/mipmap-${dir}"
+done
+
+# Associate resolutions with directory names using associative array
+declare -a dir_map
+for ((i=0; i<${#resolutions[@]}; i++))
+do
+  dir_map[${resolutions[$i]}]=${densities[$i]}
+done
+
+# Loop through resolutions, resize images, and save them in the appropriate directory
+for resolution in "${resolutions[@]}"
+do
+  dir="${dir_map[$resolution]}"
+  echo convert size ${resolution}x${resolution} for res/mipmap-${dir}
+  convert ${srcn}.png -resize ${resolution}x${resolution} -quality 100 res/mipmap-${dir}/${srcn}.webp
+  convert ${srca}.png -resize ${resolution}x${resolution} -quality 100 res/mipmap-${dir}/${srca}.webp
+done
