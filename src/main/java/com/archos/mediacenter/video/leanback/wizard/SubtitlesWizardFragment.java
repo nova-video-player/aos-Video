3978eb9994ab6b5e46f664bf26316700fa32641c
Courville Software <software@courville.org>
2024-06-16 20:41:39 +0200

    not necessary

--- src/main/java/com/archos/mediacenter/video/leanback/wizard/SubtitlesWizardFragment.java
+++ src/main/java/com/archos/mediacenter/video/leanback/wizard/SubtitlesWizardFragment.java
@@ -1,7 +1,6 @@
 package com.archos.mediacenter.video.leanback.wizard;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
