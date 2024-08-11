9b5557c98178290ca3539638b90ab4979f3f9871
Courville Software <software@courville.org>
2024-08-11 18:16:43 +0400

    VideoPreferencesCommon: add details about Chinese TMDB_LANGUAGES variants

--- src/main/java/com/archos/mediacenter/video/utils/VideoPreferencesCommon.java
+++ src/main/java/com/archos/mediacenter/video/utils/VideoPreferencesCommon.java
@@ -101,6 +101,8 @@ public class VideoPreferencesCommon implements OnSharedPreferenceChangeListener 
     // see https://developer.themoviedb.org/docs/languages
     // curl --request GET --url 'https://api.themoviedb.org/3/configuration/languages?api_key=APIKEY' | jq '.[] | .iso_639_1' | sed 's/"\([^"]*\)"/\1/g' | grep -v mo | grep -v xx | sort -u | paste -sd "|" -
     // to do after: add manually pt-br
+    // zh = Mandarin -> Chinese Simplified (zh-cn) or Chinese
+    // cn = Cantonese -> Chinese Traditional (zh-tw)
     public final static String TMDB_LANGUAGES = "aa|ab|ae|af|ak|am|an|ar|as|av|ay|az|ba|be|bg|bi|bm|bn|bo|br|bs|ca|ce|ch|cn|co|cr|cs|cu|cv|cy|da|de|dv|dz|ee|el|en|eo|es|et|eu|fa|ff|fi|fj|fo|fr|fy|ga|gd|gl|gn|gu|gv|ha|he|hi|ho|hr|ht|hu|hy|hz|ia|id|ie|ig|ii|ik|io|is|it|iu|ja|jv|ka|kg|ki|kj|kk|kl|km|kn|ko|kr|ks|ku|kv|kw|ky|la|lb|lg|li|ln|lo|lt|lu|lv|mg|mh|mi|mk|ml|mn|mr|ms|mt|my|na|nb|nd|ne|ng|nl|nn|no|nr|nv|ny|oc|oj|om|or|os|pa|pi|pl|ps|pt|pt-br|qu|rm|rn|ro|ru|rw|sa|sc|sd|se|sg|sh|si|sk|sl|sm|sn|so|sq|sr|ss|st|su|sv|sw|ta|te|tg|th|ti|tk|tl|tn|to|tr|ts|tt|tw|ty|ug|uk|ur|uz|ve|vi|vo|wa|wo|xh|yi|yo|za|zh|zu";
 
     // should we provide adaptive refresh rate for all (not only on TV)
@@ -270,7 +272,6 @@ public class VideoPreferencesCommon implements OnSharedPreferenceChangeListener 
     List<String> TMDbLanguageListEntries = new ArrayList<>();
     List<String> TMDbLanguageListEntryValues = new ArrayList<>();
     int TMDbSystemLanguageIndex = -1;
-
 
     public VideoPreferencesCommon(PreferenceFragmentCompat preferencesFragment) {
         mPreferencesFragment = preferencesFragment;
