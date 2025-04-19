5f845b7c7d62cae55d48401ca080d03774b7dbdf
matrix <austin.316scsa@yahoo.com>
2025-04-20 00:13:58 +0300

    browser list of seasons layout and font adjustments

--- src/main/java/com/archos/mediacenter/video/browser/presenter/SeasonListPresenter.java
+++ src/main/java/com/archos/mediacenter/video/browser/presenter/SeasonListPresenter.java
@@ -102,7 +102,7 @@ public class SeasonListPresenter extends SeasonPresenter{
             String seasonNumber = finalSeasonTags.get(i).getSeasonNumber();
             if (currentSeason == Integer.parseInt(seasonNumber)) {
                 holder.seasonPlot.setText(finalSeasonTags.get(i).getSeasonPlot());
-                holder.seasonPlot.setMaxLines(6);
+                holder.seasonPlot.setMaxLines(5);
                 holder.seasonAirDate.setText(finalSeasonTags.get(i).getSeasonAirdate());
                 String name = "";
                 if (finalSeasonTags.get(i).getSeasonName().isEmpty()){
