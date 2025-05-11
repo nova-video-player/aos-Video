c5ba99ad24185014f19f79ead3457f545c8ca5ad
matrix <austin.316scsa@yahoo.com>
2025-05-11 21:00:05 +0300

    browser category font and layout adjustments

--- src/main/java/com/archos/mediacenter/video/browser/BrowserCategory.java
+++ src/main/java/com/archos/mediacenter/video/browser/BrowserCategory.java
@@ -688,7 +688,7 @@ abstract public class BrowserCategory extends ListFragment {
                     tv.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
                 }
 
-                tv.setTextSize(18); // Change the value to your preferred size
+                tv.setTextSize(20); // Change the value to your preferred size
             }
             else { // This is ITEM_VIEW_TYPE_SEPARATOR
                 tv.setText((CharSequence) mCategoryList.get(position));
