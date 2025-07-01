027c33199dcb0292904eb45d8320213fe95d3316
matrix <austin.316scsa@yahoo.com>
2025-07-01 13:45:54 +0300

    Network: ShortcutRootFragment, migrate from context menu to powermenu

--- src/main/java/com/archos/mediacenter/video/browser/filebrowsing/network/ShortcutAdapter.java
+++ src/main/java/com/archos/mediacenter/video/browser/filebrowsing/network/ShortcutAdapter.java
@@ -14,10 +14,12 @@
 
 package com.archos.mediacenter.video.browser.filebrowsing.network;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -122,7 +124,9 @@ public class ShortcutAdapter extends RootFragmentAdapter {
         private Uri mUri;
         private boolean mAvailable;
         private String mName;
+        private boolean longClickHandled = false;
 
+        @SuppressLint("ClickableViewAccessibility")
         public ShortcutViewHolder(View v) {
             super(v);
             // Define click listener for the ViewHolder's View.
@@ -149,6 +153,30 @@ public class ShortcutAdapter extends RootFragmentAdapter {
             mMainTv = (TextView) v.findViewById(R.id.name);
             mSecondaryTv = (TextView) v.findViewById(R.id.second);
 
+            // get touch x and y for ShortcutRootFragment for powermenu
+            mRoot.setOnTouchListener((view, event) -> {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        view.setTag(R.id.touch_x, event.getRawX());
+                        view.setTag(R.id.touch_y, event.getRawY());
+                        longClickHandled = false; // reset
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        if (longClickHandled) {
+                            // Consume the event, don't let it perform click
+                            view.setPressed(false); // 👈 clear the pressed highlight
+                            return true;
+                        }
+                        break;
+                }
+                return false; // otherwise allow click
+            });
+            v.setOnLongClickListener(v1 -> {
+                longClickHandled = true; // mark it so touch can suppress the next click
+                v1.showContextMenu();    // trigger context menu
+                return true;             // consume the long click
+            });
         }
         public View getRoot() {
             return mRoot;
--- src/main/java/com/archos/mediacenter/video/browser/filebrowsing/network/ShortcutRootFragment.java
+++ src/main/java/com/archos/mediacenter/video/browser/filebrowsing/network/ShortcutRootFragment.java
@@ -14,16 +14,32 @@
 
 package com.archos.mediacenter.video.browser.filebrowsing.network;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.core.content.ContextCompat;
+
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.ContextMenu;
+import android.view.ContextThemeWrapper;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.archos.filecorelibrary.FileUtils;
 import com.archos.mediacenter.utils.ActionItem;
@@ -37,9 +53,16 @@ import com.archos.mediacenter.video.browser.filebrowsing.network.SmbBrowser.Brow
 import com.archos.mediacenter.video.browser.filebrowsing.network.UpnpBrowser.BrowserByUpnp;
 import com.archos.mediacenter.video.leanback.network.NetworkServerCredentialsDialog;
 import com.archos.mediaprovider.NetworkScanner;
+import com.skydoves.powermenu.MenuAnimation;
+import com.skydoves.powermenu.MenuBaseAdapter;
+import com.skydoves.powermenu.PowerMenu;
+import com.skydoves.powermenu.PowerMenuItem;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShortcutRootFragment extends NewRootFragment implements View.OnClickListener, ShortcutAdapter.OnShortcutAddListener {
 
@@ -76,12 +99,116 @@ public class ShortcutRootFragment extends NewRootFragment implements View.OnClic
         if(v.getTag() instanceof ShortcutAdapter.ShortcutViewHolder){
             mSelectedUri =((ShortcutAdapter.ShortcutViewHolder) v.getTag()).getUri();
             mSelectedName =((ShortcutAdapter.ShortcutViewHolder) v.getTag()).getName();
-            menu.add(0, R.string.remove_from_shortcuts, 0, R.string.remove_from_shortcuts);
-            menu.add(0, R.string.open_indexed_folder, 0, R.string.open_indexed_folder);
-            if(ShortcutDbAdapter.VIDEO.isShortcut(getActivity(), mSelectedUri.toString())<0)
-                menu.add(0, R.string.add_to_library, 0, R.string.add_to_library);
+            //menu.add(0, R.string.remove_from_shortcuts, 0, R.string.remove_from_shortcuts);
+            //menu.add(0, R.string.open_indexed_folder, 0, R.string.open_indexed_folder);
+            //if(ShortcutDbAdapter.VIDEO.isShortcut(getActivity(), mSelectedUri.toString())<0)
+                //menu.add(0, R.string.add_to_library, 0, R.string.add_to_library);
+
+            mTouchX = ((Float) v.getTag(R.id.touch_x)).intValue();
+            mTouchY = ((Float) v.getTag(R.id.touch_y)).intValue();
+            // Trigger haptic feedback manually on long click anchor
+            v.performHapticFeedback(android.view.HapticFeedbackConstants.LONG_PRESS);
+            showPowerMenu(v);
         }
         else super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    private void showPowerMenu(View anchor) {
+        List<PowerMenuItem> menuItems = new ArrayList<>();
+
+        menuItems.add(new PowerMenuItem(getContext().getString(R.string.remove_from_shortcuts)));
+        menuItems.add(new PowerMenuItem(getContext().getString(R.string.open_indexed_folder)));
+        if(ShortcutDbAdapter.VIDEO.isShortcut(getActivity(), mSelectedUri.toString())<0)
+            menuItems.add(new PowerMenuItem(getContext().getString(R.string.add_to_library)));
+
+        Context themedContext = new ContextThemeWrapper(getContext(), R.style.PowerMenuTheme);
+        View decorView = ((Activity) anchor.getContext()).getWindow().getDecorView();
+        int[] decorLocation = new int[2];
+        decorView.getLocationOnScreen(decorLocation);
+        int xOffset = mTouchX - decorLocation[0];
+        int yOffset = mTouchY - decorLocation[1];
+
+        String targetText = getContext().getString(R.string.remove_from_indexed_folders);
+        Typeface typeface = ResourcesCompat.getFont(getContext(), R.font.nhaasgroteskdspro_75bd);
+        float textSizeSp = 16f;
+        Resources res = getContext().getResources();
+        DisplayMetrics metrics = res.getDisplayMetrics();
+        Paint paint = new Paint();
+        paint.setTypeface(typeface);
+        paint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSizeSp, metrics));
+        float textWidth = paint.measureText(targetText);
+        int internalPaddingPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 32, metrics); // 16dp start + 16dp end
+        int externalMarginPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, metrics);
+        int menuWidth = (int) (textWidth + internalPaddingPx + externalMarginPx);
+
+        PowerMenu powerMenu = new PowerMenu.Builder(themedContext)
+                .addItemList(menuItems)
+                .setMenuRadius(16f)
+                .setMenuShadow(8f)
+                .setAnimation(MenuAnimation.DROP_DOWN)
+                .setAutoDismiss(true)
+                .setBackgroundColor(ContextCompat.getColor(getContext(), R.color.transparent))
+                .setWidth(menuWidth) // ⬅ set width here
+                .build();
+
+        // set adapter
+        ListView listView = powerMenu.getMenuListView();
+        CustomPowerMenuAdapter adapter = new CustomPowerMenuAdapter(listView);
+        adapter.addItemList(menuItems);
+        listView.setAdapter(adapter);
+
+        // set background
+        View menuListView = powerMenu.getMenuListView();
+        if (menuListView != null) {
+            View parent = (View) menuListView.getParent();
+            GradientDrawable bg = new GradientDrawable();
+            bg.setColor(ContextCompat.getColor(getContext(), R.color.tranparent_deep_blue));
+            float radiusPx = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, 12f, getContext().getResources().getDisplayMetrics());
+            int strokeWidthPx = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, 1f, getContext().getResources().getDisplayMetrics());
+            bg.setCornerRadius(radiusPx);
+            bg.setStroke(strokeWidthPx, ContextCompat.getColor(getContext(), R.color.black));
+            parent.setBackground(bg);
+        }
+
+        powerMenu.setOnMenuItemClickListener((positionClicked, item) -> {
+            String titleClicked = ((PowerMenuItem) item).title.toString();
+            handlePowerMenuClick(titleClicked);
+        });
+        powerMenu.showAtLocation(decorView, Gravity.NO_GRAVITY, xOffset, yOffset);
+    }
+
+    protected void handlePowerMenuClick(String title) {
+        if (title.equals(getContext().getString(R.string.remove_from_shortcuts))) {
+            ShortcutDb.STATIC.removeShortcut(getContext(), mSelectedUri);
+            ((ShortcutAdapter)mAdapter).updateShortcuts(ShortcutDb.STATIC.getAllShortcuts(getActivity()));
+            loadIndexedShortcuts();
+        } else if (title.equals(getContext().getString(R.string.open_indexed_folder))){
+            onShortcutTap(mSelectedUri);
+        } else if (title.equals(getContext().getString(R.string.add_to_library))){
+            addToIndexed(mSelectedUri, mSelectedName);
+        } else {
+            super.handlePowerMenuClick(title);  // fallback to base logic
+        }
+    }
+
+    public class CustomPowerMenuAdapter extends MenuBaseAdapter<PowerMenuItem> {
+        public CustomPowerMenuAdapter(ListView listView) {
+            super(listView);
+        }
+        @Override
+        public View getView(int index, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                view = inflater.inflate(R.layout.item_power_menu, parent, false);
+            }
+            PowerMenuItem item = (PowerMenuItem) getItem(index);
+            TextView title = view.findViewById(R.id.menu_item_title);
+            title.setText(((PowerMenuItem) item).title.toString());
+            return view;
+        }
     }
 
     @Override
