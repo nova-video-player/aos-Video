4ba6f757b93d86925844d0738bb7f1fbd2770699
matrix <austin.316scsa@yahoo.com>
2025-07-06 19:37:29 +0300

    BrowserPlaylists; Power menu: apply proper menu width according to the width of item with maximum width

--- src/main/java/com/archos/mediacenter/video/browser/BrowserByIndexedVideos/BrowserPlaylists.java
+++ src/main/java/com/archos/mediacenter/video/browser/BrowserByIndexedVideos/BrowserPlaylists.java
@@ -183,18 +183,23 @@ public class BrowserPlaylists extends BrowserMoviesBy {
         int xOffset = mTouchX - decorLocation[0];
         int yOffset = mTouchY - decorLocation[1];
 
-        String targetText = mContext.getString(R.string.start_auto_scraper_activity);
         Typeface typeface = ResourcesCompat.getFont(mContext, R.font.nhaasgroteskdspro_75bd);
+        if (typeface == null) typeface = Typeface.DEFAULT;
         float textSizeSp = 16f;
-        Resources res = mContext.getResources();
-        DisplayMetrics metrics = res.getDisplayMetrics();
+        DisplayMetrics metrics = mContext.getResources().getDisplayMetrics();
         Paint paint = new Paint();
         paint.setTypeface(typeface);
         paint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSizeSp, metrics));
-        float textWidth = paint.measureText(targetText);
-        int internalPaddingPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 32, metrics); // 16dp start + 16dp end
-        int externalMarginPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, metrics);
-        int menuWidth = (int) (textWidth + internalPaddingPx + externalMarginPx);
+        // Find the widest item
+        float maxTextWidth = 0;
+        for (PowerMenuItem item : menuItems) {
+            float width = paint.measureText(item.title.toString());
+            if (width > maxTextWidth) maxTextWidth = width;
+        }
+        // Calculate total width: max text + padding + margin
+        int internalPaddingPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 32, metrics); // start + end padding
+        int externalMarginPx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, metrics); // optional
+        int menuWidth = (int) (maxTextWidth + internalPaddingPx + externalMarginPx);
 
         PowerMenu powerMenu = new PowerMenu.Builder(themedContext)
                 .addItemList(menuItems)
