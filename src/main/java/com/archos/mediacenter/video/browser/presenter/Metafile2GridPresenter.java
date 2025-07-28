eef2eda3810f98690e674e07b658756f19848fb4
matrix <austin.316scsa@yahoo.com>
2025-07-29 00:44:18 +0300

    Metafile2Presenter (Folders): fix gridview (posters) stretching vertically in landscape mode by applying proper grid view measurements

--- src/main/java/com/archos/mediacenter/video/browser/presenter/Metafile2GridPresenter.java
+++ src/main/java/com/archos/mediacenter/video/browser/presenter/Metafile2GridPresenter.java
@@ -15,7 +15,15 @@
 package com.archos.mediacenter.video.browser.presenter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import androidx.preference.PreferenceManager;
+import com.archos.mediacenter.utils.ThumbnailEngine;
+import com.archos.mediacenter.video.R;
 import com.archos.mediacenter.video.browser.adapters.AdapterDefaultValuesGrid;
 
 /**
@@ -24,5 +32,81 @@ import com.archos.mediacenter.video.browser.adapters.AdapterDefaultValuesGrid;
 public class Metafile2GridPresenter extends Metafile2Presenter{
     public Metafile2GridPresenter(Context context) {
         super(context, AdapterDefaultValuesGrid.INSTANCE);
+    }
+
+    @Override
+    public View bindView(View view, final Object object, ThumbnailEngine.Result thumbnailResult, int positionInAdapter) {
+        super.bindView(view, object, thumbnailResult, positionInAdapter);
+        ViewHolder holder = (ViewHolder) view.getTag();
+
+        //set gridview thumbnail Width & Height
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        boolean drawerIsNull = prefs.getBoolean("drawerIsNull", true);
+        boolean mIsLandscapeMode = mContext.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        boolean mIsPortraitMode = mContext.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+        boolean IsTablet = mContext.getResources().getConfiguration().isLayoutSizeAtLeast(Configuration.SCREENLAYOUT_SIZE_LARGE);
+
+        //width subtraction when number of columns is 5 && mIsLandscapeMode && drawerIsNull
+        int categoryWidth = (int) mContext.getResources().getDimension(R.dimen.categories_list_width);
+        int TotalHorizontalSpacingLandscapeNullDrawer = (int) mContext.getResources().getDimension(R.dimen.total_horizontal_spacing_landscape_null_drawer);
+        int subtraction = categoryWidth + TotalHorizontalSpacingLandscapeNullDrawer;
+
+        DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+        int windowWidth = displayMetrics.widthPixels;
+        int TotalHorizontalSpacingPortrait = (int) mContext.getResources().getDimension(R.dimen.total_horizontal_spacing_portrait);
+        int TotalHorizontalSpacingLandscape = (int) mContext.getResources().getDimension(R.dimen.total_horizontal_spacing_landscape);
+
+        int TotalHorizontalSpacingTabletPortrait = (int) mContext.getResources().getDimension(R.dimen.total_horizontal_spacing_tablet_portrait);
+        int TotalHorizontalSpacingTabletLandscape = (int) mContext.getResources().getDimension(R.dimen.total_horizontal_spacing_tablet_landscape);
+        int subtractionTablet = categoryWidth + TotalHorizontalSpacingTabletLandscape;
+        int subtractionTabletPortrait = categoryWidth + TotalHorizontalSpacingTabletPortrait;
+
+        int width;
+        if(!IsTablet){
+            if(mIsPortraitMode){
+                width = windowWidth - TotalHorizontalSpacingPortrait;
+            }else if(mIsLandscapeMode && drawerIsNull){
+                width = windowWidth - subtraction;
+            }else{
+                width = windowWidth - TotalHorizontalSpacingLandscape;
+            }
+        }else{
+            if(mIsLandscapeMode){
+                width = windowWidth - subtractionTablet;
+            }else{
+                if(drawerIsNull){
+                    width = windowWidth - subtractionTabletPortrait;
+                }else{
+                    width = windowWidth - TotalHorizontalSpacingTabletPortrait;
+                }
+            }
+        }
+
+        int columnWidth;
+        if(!IsTablet){
+            if(mIsPortraitMode){
+                columnWidth = width / 3 ;
+            }else if(mIsLandscapeMode && drawerIsNull){
+                columnWidth = width / 5 ;
+            }else{
+                columnWidth = width / 6 ;
+            }
+        }else{
+            if(mIsLandscapeMode){
+                columnWidth = width / 8;
+            }else{
+                if(drawerIsNull){
+                    columnWidth = width / 4;
+                }else{
+                    columnWidth = width / 5;
+                }
+            }
+        }
+
+        int height = columnWidth / 2;
+        int columnHeight = height * 3;
+        holder.thumbnail.setLayoutParams(new RelativeLayout.LayoutParams(columnWidth, columnHeight));
+
+        return view;
     }
 }
