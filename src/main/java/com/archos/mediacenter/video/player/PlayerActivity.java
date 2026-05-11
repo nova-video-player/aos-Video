b5c0d5401e8d4186d85e991dcc4ad9072076531d
Courville Software <software@courville.org>
2026-05-11 19:04:40 +0200

    do not force dynamic delay off to avos so that pcm fallback from passthrough will honor the option

--- src/main/java/com/archos/mediacenter/video/player/PlayerActivity.java
+++ src/main/java/com/archos/mediacenter/video/player/PlayerActivity.java
@@ -942,7 +942,7 @@ public class PlayerActivity extends AppCompatActivity implements PlayerControlle
             LibAvos.enableAudioSpeed(mPreferences.getBoolean(KEY_PLAYBACK_SPEED,false));
             LibAvos.disableAtempoFilter(mPreferences.getBoolean(VideoPreferencesCommon.KEY_AUDIO_SPEED_AUDIOTRACK, false));
             LibAvos.setAudioSpeed(audioSpeed); // set audio speed playback (does nothing if audio speed not enabled)
-            LibAvos.setDynamicAudioDelay(passthroughMode == 0 && mPreferences.getBoolean(VideoPreferencesCommon.KEY_ENABLE_DYNAMIC_AUDIO_DELAY, true)); // set dynamic audio delay estimation (default enabled)
+            LibAvos.setDynamicAudioDelay(mPreferences.getBoolean(VideoPreferencesCommon.KEY_ENABLE_DYNAMIC_AUDIO_DELAY, true)); // AVOS applies it only when the active sink path can use dynamic delay.
             LibAvos.parserSyncMode(Integer.parseInt(mPreferences.getString(KEY_PARSER_SYNC_MODE,"0"))); // set lavc parser sync mode (0: PTS, 1 samples)
             applyDownmixPreferenceToAvos();
         }
