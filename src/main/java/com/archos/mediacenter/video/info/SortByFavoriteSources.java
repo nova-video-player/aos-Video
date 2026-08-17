c1abc5689fcb98179b49de0fcf6535cb07b322fe
Courville Software <software@courville.org>
2026-08-17 19:09:13 +0200

    SortByFavoriteSources: fix Comparator contract violation
    SortByFavoriteSources.compare() violated the Java Comparator contract (symmetry and reflexivity) by unconditionally returning -1 when elements shared the same local/remote status or unhandled resolution rank, and by not properly handling elements missing from oldVideoList. This caused TimSort to throw an IllegalArgumentException ("Comparison method violates its general contract!") when sorting video sources in VideoDetailsFragment and VideoInfoActivityFragment.
    
    Refactor SortByFavoriteSources to enforce strict total ordering:
    - Handle nulls and identity (return 0 when video1 == video2)
    - Compare positions in mOldVideoList (preferring previously selected videos)
    - Prefer 3D over non-3D
    - Compare definition ranks (4K > 1080P > 720P > SD > UNKNOWN)
    - Compare location ranks (Local > Fast Remote > Slow Remote)
    - Fallback to stable URI string comparison
    
    See https://github.com/nova-video-player/aos-AVP/issues/1877

--- src/main/java/com/archos/mediacenter/video/info/SortByFavoriteSources.java
+++ src/main/java/com/archos/mediacenter/video/info/SortByFavoriteSources.java
@@ -14,6 +14,8 @@
 
 package com.archos.mediacenter.video.info;
 
+import android.net.Uri;
+
 import com.archos.filecorelibrary.FileUtils;
 import com.archos.mediacenter.video.browser.adapters.object.Video;
 import com.archos.mediaprovider.video.VideoStore;
@@ -24,7 +26,7 @@ import java.util.List;
 /**
  * Created by alexandre on 08/12/15.
  */
-public class SortByFavoriteSources  implements Comparator<Video> {
+public class SortByFavoriteSources implements Comparator<Video> {
     private final List<Video> mOldVideoList;
 
     public SortByFavoriteSources(List<Video> oldVideoList) {
@@ -32,65 +34,94 @@ public class SortByFavoriteSources  implements Comparator<Video> {
     }
 
     @Override
-        public int compare(Video video1, Video video2) {
-        int pos1=0;
-        int pos2=0;
-        boolean found1 = false;
-        boolean found2 = false;
-        for(Video oldVideoInList : mOldVideoList){
-            if(oldVideoInList.getUri().equals(video1.getUri())){
-                found1 = true;
-                if(found2)
-                    break;
-            }
-            if(oldVideoInList.getUri().equals(video2.getUri())){
-                found2 = true;
-                if(found1)
-                    break;
-            }
-            if(!found1)
-                pos1++;
-            if(!found2)
-                 pos2++;
+    public int compare(Video video1, Video video2) {
+        if (video1 == video2) {
+            return 0;
         }
-        if(found1&&found2){
-            if(pos1<pos2)
-                return -1;
-            if(pos1>pos2)
-                return 1;
+        if (video1 == null) {
+            return 1;
         }
-            if(video1.is3D()&&!video2.is3D())
-                return -1;
-            if(!video1.is3D()&&video2.is3D())
-                return 1 ;
-            if(video1.getNormalizedDefinition()!=video2.getNormalizedDefinition()){ // return better resolution
-
-                if(video1.getNormalizedDefinition() == VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_4K)
-                    return -1;
-                if(video2.getNormalizedDefinition() == VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_4K)
-                    return 1;
-                if(video1.getNormalizedDefinition() == VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_1080P)
-                    return -1;
-                if(video2.getNormalizedDefinition() == VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_1080P)
-                    return 1;
-                if(video1.getNormalizedDefinition() == VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_720P)
-                    return -1;
-                if(video2.getNormalizedDefinition() == VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_720P)
-                    return 1;
-
-            }
-
-            if(FileUtils.isLocal(video1.getFileUri()))
-                return -1;
-            if (FileUtils.isLocal(video2.getFileUri()))
-                return 1;
-            if(!FileUtils.isSlowRemote(video1.getFileUri()))
-                return -1;
-            if(!FileUtils.isSlowRemote(video2.getFileUri()))
-                return 1;
-
-
+        if (video2 == null) {
             return -1;
+        }
 
+        if (mOldVideoList != null && !mOldVideoList.isEmpty()) {
+            int pos1 = -1;
+            int pos2 = -1;
+            int index = 0;
+            for (Video oldVideoInList : mOldVideoList) {
+                if (oldVideoInList != null && oldVideoInList.getUri() != null) {
+                    if (pos1 == -1 && oldVideoInList.getUri().equals(video1.getUri())) {
+                        pos1 = index;
+                    }
+                    if (pos2 == -1 && oldVideoInList.getUri().equals(video2.getUri())) {
+                        pos2 = index;
+                    }
+                    if (pos1 != -1 && pos2 != -1) {
+                        break;
+                    }
+                }
+                index++;
+            }
+            if (pos1 != -1 && pos2 != -1) {
+                if (pos1 != pos2) {
+                    return Integer.compare(pos1, pos2);
+                }
+            } else if (pos1 != -1) {
+                return -1;
+            } else if (pos2 != -1) {
+                return 1;
+            }
+        }
+
+        if (video1.is3D() != video2.is3D()) {
+            return video1.is3D() ? -1 : 1;
+        }
+
+        int def1 = getDefinitionRank(video1.getNormalizedDefinition());
+        int def2 = getDefinitionRank(video2.getNormalizedDefinition());
+        if (def1 != def2) {
+            return Integer.compare(def2, def1);
+        }
+
+        int loc1 = getLocationRank(video1.getFileUri());
+        int loc2 = getLocationRank(video2.getFileUri());
+        if (loc1 != loc2) {
+            return Integer.compare(loc2, loc1);
+        }
+
+        if (video1.getFileUri() != null && video2.getFileUri() != null) {
+            return video1.getFileUri().toString().compareTo(video2.getFileUri().toString());
+        }
+        return 0;
+    }
+
+    private static int getDefinitionRank(int definition) {
+        switch (definition) {
+            case VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_4K:
+                return 4;
+            case VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_1080P:
+                return 3;
+            case VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_720P:
+                return 2;
+            case VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_SD:
+                return 1;
+            case VideoStore.Video.VideoColumns.ARCHOS_DEFINITION_UNKNOWN:
+            default:
+                return 0;
+        }
+    }
+
+    private static int getLocationRank(Uri uri) {
+        if (uri == null) {
+            return 0;
+        }
+        if (FileUtils.isLocal(uri)) {
+            return 2;
+        }
+        if (!FileUtils.isSlowRemote(uri)) {
+            return 1;
+        }
+        return 0;
     }
 }
